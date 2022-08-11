3f121ec78c6593a3a83008fe769f80514d0c569e
Daniel Apatin <daniel@apatin.ru>
2022-08-11 23:36:21 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,82 @@
 # LRU Cache
 
 A simple LRU cache using go generics.
+
+It automatically removes elements as new elements are added if the capacity is reached. Items are removes based on how recently they were used where the oldest items are removed first.
+
+## CONSTANTS
+
+```go
+const (
+        // DefaultCacity is the default cache capacity
+        DefaultCapacity = 10000
+)
+```
+
+## TYPES
+
+```go
+type Cache[K comparable, V any] struct {}
+```
+
+## FUNCTIONS
+
+New initializes a new lru cache with the given capacity.
+
+```go
+func New[K comparable, V any](cacheOptions ...CacheOption) *Cache[K, V]
+```
+
+Delete an item from the cache.
+
+```go
+func (c *Cache[K, V]) Delete(key K) bool
+```
+
+Flush deletes all items from the cache.
+
+```go
+func (c *Cache[K, V]) Flush()
+```
+
+Get an item from the cache. This operation updates recent usage of the item.
+
+```go
+func (c *Cache[K, V]) Get(key K) (value V, ok bool)
+```
+
+Keys returns a slice of all the keys in the cache.
+
+```go
+func (c *Cache[K, V]) Keys() []K
+```
+
+Len is the number of key value pairs in the cache.
+
+```go
+func (c *Cache[K, V]) Len() int
+```
+
+Peek gets an item from the cache without updating the recent usage.
+
+```go
+func (c *Cache[K, V]) Peek(key K) (value V, ok bool)
+```
+
+Set the given key value pair. This operation updates the recent usage of the item.
+
+```go
+func (c *Cache[K, V]) Set(key K, value V)
+```
+
+### CacheOption configurs a lru cache.
+
+```go
+type CacheOption interface {}
+```
+
+WithCapacity configures how many items can be stored before old items begin to be deleted.
+
+```go
+func WithCapacity(capacity int) CacheOption
+```
---
+++ sync.go
@@ -0,0 +1,73 @@
+package lru
+
+import (
+	"sync"
+
+	"github.com/ad/lru/internal"
+)
+
+var _ internal.LRU[int, int] = &SyncCache[int, int]{}
+
+// SyncCache is a threadsafe lru cache.
+type SyncCache[K comparable, V any] struct {
+	cache *Cache[K, V]
+	mu    sync.RWMutex
+}
+
+// New initializes a new lru cache with the given capacity.
+func NewSync[K comparable, V any](options ...CacheOption) *SyncCache[K, V] {
+	return &SyncCache[K, V]{
+		cache: New[K, V](options...),
+	}
+}
+
+// Len is the number of key value pairs in the cache.
+func (c *SyncCache[K, V]) Len() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.cache.Len()
+}
+
+// Set the given key value pair.
+// This operation updates the recent usage of the item.
+func (c *SyncCache[K, V]) Set(key K, value V) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache.Set(key, value)
+}
+
+// Get an item from the cache.
+// This operation updates recent usage of the item.
+func (c *SyncCache[K, V]) Get(key K) (value V, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.cache.Get(key)
+}
+
+// Peek gets an item from the cache without updating the recent usage.
+func (c *SyncCache[K, V]) Peek(key K) (value V, ok bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.cache.Peek(key)
+}
+
+// Delete an item from the cache.
+func (c *SyncCache[K, V]) Delete(key K) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.cache.Delete(key)
+}
+
+// Flush deletes all items from the cache.
+func (c *SyncCache[K, V]) Flush() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache.Flush()
+}
+
+// Keys returns a slice of all the keys in the cache.
+func (c *SyncCache[K, V]) Keys() []K {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.cache.Keys()
+}
